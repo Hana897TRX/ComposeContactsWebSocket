3194789f6dc8c37495dd264329c07ed0cfe0aafa
Hana897TRX <hana897trx@gmail.com>
2023-05-10 05:18:53 +0900

    Socket reconnect

--- app/src/main/java/com/hana897trx/contactsregister/data/socket/SocketRDSI.kt
+++ app/src/main/java/com/hana897trx/contactsregister/data/socket/SocketRDSI.kt
@@ -3,8 +3,10 @@ package com.hana897trx.contactsregister.data.socket
 import com.google.gson.Gson
 import com.hana897trx.contactsregister.data.socket.listener.WebSocketHana
 import com.hana897trx.contactsregister.data.socket.model.SocketPayload
+import com.hana897trx.contactsregister.utils.Constants.WEB_SOCKET_URL
 import com.hana897trx.contactsregister.utils.ResourceState
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
@@ -12,15 +14,23 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.onEach
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.WebSocket
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class SocketRDSI @Inject constructor(
-    private val webSocketListener: WebSocketHana,
-    private val webSocket: WebSocket,
+    private var webSocketListener: WebSocketHana,
 ): SocketRDS {
+    private var client: OkHttpClient = OkHttpClient()
+    private var request: Request = Request
+    .Builder()
+    .url(WEB_SOCKET_URL)
+    .build()
+
+    private var webSocket: WebSocket = client.newWebSocket(request, webSocketListener)
 
     override suspend fun sendData(socketPayload: SocketPayload) : Flow<ResourceState<Boolean>> = flow {
         emit(ResourceState.Loading)
@@ -28,6 +38,9 @@ class SocketRDSI @Inject constructor(
             webSocketListener.onMessage(webSocket, Gson().toJson(socketPayload))
             emit(ResourceState.Success(true))
         } catch (e: Exception) {
+            webSocket.close(1000, null)
+            delay(1000)
+            webSocket = client.newWebSocket(request, webSocketListener)
             emit(ResourceState.Error(errorMessage = e.message ?: "Unknown error"))
         }
     }.flowOn(IO)
